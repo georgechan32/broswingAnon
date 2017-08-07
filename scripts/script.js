df5a939d95c37da9c0fe0769c6322610da77ac1e
george.chan <george.chan@accenture.com>
2017-08-06 22:44:07 -0700

    Add jquery

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,6 @@
 // Main javascript entry point
 // Should handle bootstrapping/starting application
 
-'use strict';
-
-var $ = require('jquery');
-var Link = require('../_modules/link/link');
 
 $(function() {
 
